a27335adeaf05d039a1cb5b4a295e6c22b70cb60
Olayiwola <olayiwola@gmail.com>
2024-09-05 13:02:05 +0100

    first commit

--- index.js
+++ index.js
@@ -36,3 +36,65 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+// Set the current year in the span element
+document.getElementById('current-year').textContent = currentYear;
+
+ // Back to Top function
+mybutton = document.getElementById("back-to-top-button");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Contact form
+var form = document.getElementById("contact-form");
+  
+function clearForm(event) {
+  // Prevent the default form submission behavior to process the clearing
+  event.preventDefault();
+
+  // Optionally, send the form data to Formspree before clearing the form
+  const form = event.target;
+  fetch(form.action, {
+    method: form.method,
+    body: new FormData(form),
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      // Clear all fields after successful submission
+      form.reset();
+      alert("Form submitted successfully!");
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          alert(data["errors"].map(error => error["message"]).join(", "))
+        } else {
+          alert("Oops! Form submission failed. Please try again.");
+        }
+      })
+    }
+  }).catch(error => {
+    console.error('Error:', error);
+    alert("There was an error submitting the form.");
+  });
+}
---
+++ start.sh
@@ -0,0 +1,4 @@
+nvm use 16 && npm run compile:scss
+
+# Listen to changes in CSS Preprocessor files ( SASS files )
+live-server
